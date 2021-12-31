bd91d867fd17f950194ee7422919895aba2fbf2d
plquocthang2008110216 <96822204+plquocthang2008110216@users.noreply.github.com>
2021-12-31 12:08:04 +0700

    Update Node.java

--- src/ThiCuoiKi/Node.java
+++ src/ThiCuoiKi/Node.java
@@ -1,1 +1,31 @@
+gói  ThiCuoiKi ;
 
+ Node lớp  công khai {
+    public   HangHoa hangHoa;
+    công khai   Node tiếp theo;
+     Node công cộng ()
+    {
+
+    }
+    public  Node ( HangHoa  hangHoa )
+    {
+        cái này . hangHoa = hangHoa;
+        cái này . tiếp theo =  null ;
+    }
+    public  HangHoa  getHangHoa () {
+        return hangHoa;
+    }
+    public  void  setHangHoa ( HangHoa  hangHoa ) {
+        cái này . hangHoa = hangHoa;
+    }
+    public  Node  getNext () {
+        trở lại tiếp theo;
+    }
+    public  void  setNext ( Node  next ) {
+        cái này . next = tiếp theo;
+    }
+    public  Node ( HangHoa  hangHoa , Node  next ) {
+        cái này . hangHoa = hangHoa;
+        cái này . next = tiếp theo;
+    }
+}
